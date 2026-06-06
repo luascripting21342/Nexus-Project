9b58e7cc9c656801b6ad90b3b0017a53b004f0e1
siqsoqcoder <ahmetae132js@gmail.com>
2026-06-06 04:49:46 +0300

    deneme

---
+++ deneme.bat
@@ -0,0 +1,9 @@
+@echo off
+title Sistem Analizi
+echo ==========================================
+echo SİSTEM İHLAL TESTİ BAŞARILI
+echo ==========================================
+echo Bu dosya GitHub'dan basariyla indirildi ve calistirildi.
+echo.
+pause
+exit
