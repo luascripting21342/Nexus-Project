84d1e1ec9cf9bd09268b8abce493e23777d98b60
siqsoqcoder <ahmetae132js@gmail.com>
2026-07-23 09:32:32 +0300

    Update script URL in allah.lua

--- allah.lua
+++ allah.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/8c252b5211d46319d9e272bc0f7b9f6416a482e715006571857b29d1aa47c7b9/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/7a932e84eda80a11429aab7762d502b933448ee1dc66d829694b636225cc1611/download"))()
